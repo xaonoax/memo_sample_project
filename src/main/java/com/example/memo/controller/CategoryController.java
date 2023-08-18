284d54d5df715d07749bbc55e002cb0ea19b8bc9
Ji-0 <xaonoax@gmail.com>
2023-08-19 02:08:53 +0900

    카테고리 컨트롤러 수정 -카테고리 서비스를 활용하도록 추가

--- src/main/java/com/example/memo/controller/CategoryController.java
+++ src/main/java/com/example/memo/controller/CategoryController.java
@@ -1,6 +1,9 @@
 package com.example.memo.controller;
 
 import com.example.memo.dto.CategoryDTO;
+import com.example.memo.entity.Category;
+import com.example.memo.service.CategoryService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController  // 모든 데이터는 요청과 응답의 body를 json으로 전송하게 한다.
                  // 별다른 어노테이션 없이 메서드에 객체를 전달할 시 json으로 응답한다.
 @RequestMapping("/categories")
+@AllArgsConstructor
 public class CategoryController {
+    private final CategoryService categoryService;
+
     @PostMapping("")
-    public CategoryDTO createCategory(@RequestBody CategoryDTO categoryDTO) {  // HTTP 요청의 body부분을 가져온 후,
+    public Category createCategory(@RequestBody CategoryDTO categoryDTO) {  // HTTP 요청의 body부분을 가져온 후,
                                                                                // body의 내용을 Category DTO에 담아서 처리함
-        return categoryDTO;  // 자동으로 json으로 응답
+        return categoryService.createCategory(categoryDTO);  // 자동으로 json으로 응답
     }
 }
